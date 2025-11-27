9067502213730318a7c049f076e4228e3d971224
Mathew-Samir <mthwsamir@gmail.com>
2025-11-27 23:32:28 +0200

    feat: 🎸 shared component button

--- src/app/feature/home/home.ts
+++ src/app/feature/home/home.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { Buttons } from '../../shared/components/ui/buttons/buttons';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Buttons],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
-
+  isLoading = signal<boolean>(false);
+  submit() {
+    this.isLoading.set(true);
+  }
 }
