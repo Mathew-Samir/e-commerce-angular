d0481004181bffe284324f5913ee54722899fff7
Mathew-Samir <mthwsamir@gmail.com>
2025-12-22 17:31:05 +0200

    fix: 🐛 fix routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,13 +5,44 @@ import { Cart } from './feature/cart/cart';
 import { Categories } from './feature/categories/categories';
 import { Brands } from './feature/brands/brands';
 import { Products } from './feature/products/products';
+import { Auth } from './layout/auth/auth';
+import { Dashboard } from './layout/dashboard/dashboard';
+import { Signup } from './layout/auth/components/signup/signup';
+import { Login } from './layout/auth/components/login/login';
+import { ResetPassword } from './layout/auth/components/reset-password/reset-password';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: Home },
-  { path: 'cart', component: Cart },
-  { path: 'categories', component: Categories },
-  { path: 'brands', component: Brands },
-  { path: 'products', component: Products },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      { path: 'home', component: Home },
+      { path: 'cart', component: Cart },
+      { path: 'categories', component: Categories },
+      { path: 'brands', component: Brands },
+      { path: 'products', component: Products },
+    ],
+  },
+
+  {
+    path: 'auth',
+    component: Auth,
+    children: [
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+      { path: 'login', component: Login },
+      { path: 'signup', component: Signup },
+      { path: 'reset-password', component: ResetPassword },
+    ],
+  },
   { path: '**', component: NotFound },
 ];
--- src/app/layout/dashboard/dashboard.ts
+++ src/app/layout/dashboard/dashboard.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-export class Dashboard {
-
-}
+export class Dashboard {}
